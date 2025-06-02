a7192f331d3f9b9fcf7f27c58eaba5a60b03a757
Toan Quoc Tran Doan <95047974+To4z@users.noreply.github.com>
2025-06-03 01:41:11 +0700

    Re-add yolov5 as full local folder (no submodule)

--- app.py
+++ app.py
@@ -22,4 +22,5 @@ def detect_plate():
     return jsonify({'plates': plates})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+    # app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
